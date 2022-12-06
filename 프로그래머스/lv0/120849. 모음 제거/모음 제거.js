9c659d031068b07817c26dce1653af9495d6f2b0
yjyaang <116554914+yjyaang@users.noreply.github.com>
2022-12-06 15:11:33 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = '';
+    var vowel = 'aeiou';
+    // 반복문: my_string의 모든 인덱스 순회
+    // 해당 인덱스가 vowel에 없을 때 answer에 추가
+    for(let i=0; i<my_string.length; i++) {
+        if(vowel.indexOf(my_string[i]) === -1) {
+            answer += my_string[i];
+        }
+    }
+    return answer;
+}
