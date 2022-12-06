3d731e027352dac6ace470cbb051fe030e8f2d1a
yjyaang <116554914+yjyaang@users.noreply.github.com>
2022-12-06 15:14:03 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,12 +1,15 @@
 function solution(my_string) {
-    var answer = '';
-    var vowel = 'aeiou';
-    // 반복문: my_string의 모든 인덱스 순회
-    // 해당 인덱스가 vowel에 없을 때 answer에 추가
-    for(let i=0; i<my_string.length; i++) {
-        if(vowel.indexOf(my_string[i]) === -1) {
-            answer += my_string[i];
-        }
-    }
-    return answer;
+    // var answer = '';
+    // var vowel = 'aeiou';
+    // // 반복문: my_string의 모든 인덱스 순회
+    // // 해당 인덱스가 vowel에 없을 때 answer에 추가
+    // for(let i=0; i<my_string.length; i++) {
+    //     if(vowel.indexOf(my_string[i]) === -1) {
+    //         answer += my_string[i];
+    //     }
+    // }
+    // return answer;
+    
+    //arr.filter사용
+    return my_string.split('').filter(el => 'aeiou'.indexOf(el) === -1).join('')
 }
