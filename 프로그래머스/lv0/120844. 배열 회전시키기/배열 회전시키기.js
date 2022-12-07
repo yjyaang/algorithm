4369dd4665cee3a4a68af8c8d1b511eaf0795f11
yjyaang <116554914+yjyaang@users.noreply.github.com>
2022-12-07 15:09:59 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,5 +1,6 @@
 function solution(numbers, direction) {
     var answer = [];
+    // pop, shift의 리턴값이 '빠진 값'인 것을 활용하면 코드를 좀 더 간결하게 작성 가능
     // right일 경우 변수에 마지막인덱스 할당, pop->unshift
     if(direction === "right") {
         numbers.unshift(numbers.pop());
