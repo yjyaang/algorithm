3ebac99b643da9cc30c30ee60f556c10c4a7ac5b
yjyaang <116554914+yjyaang@users.noreply.github.com>
2022-12-21 15:21:38 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -4,12 +4,15 @@ function solution(chicken) {
     // 10을 10으로 나누면 몫 1 나머지 0
     let answer = 0;
     let coupon = 0;
+    // 10마리 미만이 될 때까지 반복문
     while(chicken >= 10) {
         answer = answer + Math.floor(chicken/10);
         coupon = coupon + chicken % 10;
         chicken = Math.floor(chicken/10);
     }
+    // 10마리 미만일 시 쿠폰에 치킨 수만큼 더해야 함(서비스 치킨에 대한 쿠폰이 나왔으나 10장 미만이라 어쩌지 못하는 상태)
     coupon = coupon + chicken;
+    // 쿠폰이 10장 이상일 경우 쿠폰이 10장 미만이 되어 못 쓰게 될 때까지 서비스 치킨 주문 가능
     if(coupon >= 10) {
         while(coupon>=10) {
         answer = answer + Math.floor(coupon/10);
