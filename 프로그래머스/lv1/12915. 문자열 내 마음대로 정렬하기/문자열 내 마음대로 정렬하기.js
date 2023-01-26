e58e83db786ba38fac05374a73ac340e2248b0ba
Yejin Yang <116554914+yjyaang@users.noreply.github.com>
2023-01-26 21:15:19 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,8 +1,9 @@
 function solution(strings, n) {
-    // n번째 인덱스로 strings를 바꾸기
-    // sort 해서 정렬하기 -> 다시 원래대로 바꾸기
+    // sort 사용 -> sort 안의 조건을 짜주기
     strings.sort((a,b) => {
+        // 리턴이 0보다 작을 경우 a가 앞 => 오름차순 정렬
         if(a[n] < b[n]) return -1;
+        // 리턴이 0보다 클 경우 b가 앞
         if(a[n] > b[n]) return 1;
         // 두 문자가 같은 경우에는 사전순으로 앞선 문자열이 앞쪽에 위치
         if(a[n] === b[n]) {
@@ -11,6 +12,7 @@ function solution(strings, n) {
             while(a[idx] === b[idx]) {
                 idx++;
             }
+            // 차이날 경우 위에서 사용했던 오름차순 정렬 방법을 다시 활용
             if(a[idx] < b[idx]) return -1;
             if(a[idx] > b[idx]) return 1;
         }
